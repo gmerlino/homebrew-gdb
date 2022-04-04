e0783dc4c67a54f95407ee3d6e2234a1f4575c7c
Giovanni Merlino <gmerlino@unime.it>
2022-04-05 01:51:17 +0200

    Initial commit

---
+++ Formula/gdb.rb
@@ -0,0 +1,66 @@
+class Gdb < Formula
+  desc "GNU debugger"
+  homepage "https://www.gnu.org/software/gdb/"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-11.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-11.2.tar.xz"
+  sha256 "1497c36a71881b8671a9a84a0ee40faab788ca30d7ba19d8463c3cc787152e32"
+  license "GPL-3.0-or-later"
+  head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
+
+  depends_on "gmp"
+  depends_on "python@3.10"
+  depends_on "xz" # required for lzma support
+
+  uses_from_macos "texinfo" => :build
+  uses_from_macos "expat"
+  uses_from_macos "ncurses"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "gcc"
+    depends_on "guile"
+  end
+
+  fails_with :clang do
+    build 800
+    cause <<~EOS
+      probe.c:63:28: error: default initialization of an object of const type
+      'const any_static_probe_ops' without a user-provided default constructor
+    EOS
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    args = %W[
+      --enable-targets=all
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --with-lzma
+      --with-python=#{Formula["python@3.10"].opt_bin}/python3
+      --disable-binutils
+    ]
+
+    mkdir "build" do
+      system "../configure", *args
+      system "make"
+
+      # Don't install bfd or opcodes, as they are provided by binutils
+      system "make", "install-gdb", "maybe-install-gdbserver"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      gdb requires special privileges to access Mach ports.
+      You will need to codesign the binary. For instructions, see:
+
+        https://sourceware.org/gdb/wiki/PermissionsDarwin
+    EOS
+  end
+
+  test do
+    system bin/"gdb", bin/"gdb", "-configuration"
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew formula for GDB
+## How do I install this formula?
+`brew tap gmerlino/gdb` and then `brew install <gdb>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/gmerlino/homebrew-gdb/master/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://github.com/Homebrew/brew/tree/master/share/doc/homebrew#readme).
